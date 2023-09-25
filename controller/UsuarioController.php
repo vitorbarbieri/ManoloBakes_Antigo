b8edf6660f0c2dc6c83abb7db156d9c9d7ca6bfb
vitorbarbieri <vbarbierisilva@gmail.com>
2023-09-25 16:34:37 -0300

    Criar tela usuario

---
+++ controller/UsuarioController.php
@@ -0,0 +1,18 @@
+<?php
+
+class UsuarioController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function usuario()
+    {
+        $data['page_id'] = 3;
+        $data['page_tag'] = "Usuário - Manolo Bakes";
+        $data['page_title'] = "Usuário";
+        $data['page_name'] = "usuario";
+        $this->views->getView($this, "usuario", $data);
+    }
+}
---
+++ view/usuario/usuario.php
@@ -0,0 +1,26 @@
+<?= headerAdmin($data); ?>
+<?= navAdmin($data); ?>
+
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1>
+                <i class="bi bi-speedometer"></i>&nbsp;
+                <?= $data['page_title'] ?>
+            </h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
+            <li class="breadcrumb-item"><a href="<?= base_url() . '/' . $data['page_name']; ?>"><?= $data['page_name']; ?></a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                <div class="tile-body">Create a beautiful dashboard</div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?= footerAdmin($data); ?>
